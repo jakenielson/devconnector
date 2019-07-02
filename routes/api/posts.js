492f97926869a8eccdd771a10996c3da29090191
Jake <jakenielsonweb@gmail.com>
2019-07-02 01:31:52 -0700

    add and remove comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -180,6 +180,95 @@ router.put('/:id/unlike', auth, async (req, res) => {
 
     return res.status(500).send('Server Error');
   }
+});
+
+// @route   POST api/posts/:id/comment
+// @desc    Create a comment
+// @access  Private
+router.post('/:id/comment', [
+  auth,
+  [
+    check(
+      'text',
+      'Text is required'
+    ).not().isEmpty()
+  ]
+], async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    const newCmt = {
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    };
+
+    post.comments.unshift(newCmt);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    } 
+
+    return res.status(500).send('Server Error');
+  }
+});
+
+// @route   DELETE api/posts/:post_id/comment/:comment_id
+// @desc    Create a comment
+// @access  Private
+router.delete('/:post_id/comment/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment not found' });
+    }
+
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    // Get remove index
+    const removeIndex = post.comments.map(comment => comment.id).indexOf(req.params.comment_id);
+
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Comment not found' });
+    } 
+    
+    return res.status(500).send('Server Error');
+  }
 })
 
 module.exports = router;
